07708ae59197c450469e33872a32263de6c3eafc
Adelita Martinez <amartinez1013@cnm.edu>
2024-07-25 18:12:47 -0600

    Added last function

--- database.py
+++ database.py
@@ -15,11 +15,28 @@ def init_db():
     conn.commit()
     conn.close()
 
-    def add_transaction_to_db(transaction):
+def add_transaction_to_db(transaction):
     # Function to add a transaction to the database
-     conn = sqlite3.connect('finance.db')
+    conn = sqlite3.connect('finance.db')
     cursor = conn.cursor()
     cursor.execute("INSERT INTO transactions (amount, type, date, description) VALUES (?, ?, ?, ?)",
                    (transaction['amount'], transaction['type'], transaction['date'], transaction['description']))
     conn.commit()
     conn.close()
+
+def delete_transaction_from_db(transaction_id):
+    # Function to delete a transaction from the database
+    conn = sqlite3.connect('finance.db')
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM transactions WHERE id=?", (transaction_id,))
+    conn.commit()
+    conn.close()
+
+def get_all_transactions():
+    # Function to get all transactions from the database
+    conn = sqlite3.connect('finance.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM transactions")
+    transactions = cursor.fetchall()
+    conn.close()
+    return transactions
